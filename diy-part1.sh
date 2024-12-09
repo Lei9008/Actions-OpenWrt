376134e1385b95834ebd144e3a9501e95a43142a
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2024-12-09 09:08:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# Add a feed source 添加额外的软件包源
+
+echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
