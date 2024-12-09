96d77cfc83c3c1e213eb1fb8b7f5dafd75bfa1c3
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2024-12-09 13:26:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,6 @@
 echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git smpackage https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+sed -i '$a src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
 
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
