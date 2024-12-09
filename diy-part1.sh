83c79218663dce13f16247b723a357b5b28c32f5
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2024-12-09 17:51:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,9 +15,9 @@
 
 # Add a feed source 添加额外的软件包源
 
-echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
-echo 'src-git smpackage https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-sed -i '$a src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
+# echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+# echo 'src-git smpackage https://github.com/kenzok8/small' >>feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# sed -i '$a src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
 
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
